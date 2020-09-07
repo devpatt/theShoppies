05152f82891309e9e45d7430ccd2f06f88caafe0
Deven <devenpattni@hotmail.com>
2020-09-07 17:58:46 -0400

    Updates

---
+++ precache-manifest.4532dd704488ff0151e40de4107ab558.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f7b26819d5f59a6c2786742a96ebaa",
+    "url": "/theShoppies/index.html"
+  },
+  {
+    "revision": "b4d0a807696907bcc20c",
+    "url": "/theShoppies/static/css/main.5d82d638.chunk.css"
+  },
+  {
+    "revision": "b070a3cc41b8d8bf5716",
+    "url": "/theShoppies/static/js/2.1db040fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/theShoppies/static/js/2.1db040fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d0a807696907bcc20c",
+    "url": "/theShoppies/static/js/main.f907fe20.chunk.js"
+  },
+  {
+    "revision": "7297c4875363729728c3",
+    "url": "/theShoppies/static/js/runtime-main.d4fede78.js"
+  }
+]);
